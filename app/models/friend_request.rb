c6ef5861dbb62e3dcfd89fe300d320ca3b5aa98a
github username <github email address>
2019-10-24 11:05:13 -0500

    modified files

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,27 +1,27 @@
-class FriendRequest < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: 'User'
+# class FriendRequest < ApplicationRecord
+#   belongs_to :user
+#   belongs_to :friend, class_name: 'User'
 
-  validates :user, presence: true
-  validates :friend, presence: true, uniqueness: { scope: :user }
+#   validates :user, presence: true
+#   validates :friend, presence: true, uniqueness: { scope: :user }
 
 
-  validate :not_friends
-  validate :not_pending
+#   validate :not_friends
+#   validate :not_pending
   
-  private
+#   private
 
-  def not_friends
-    errors.add(:friend, 'is already added') if user.friends.include?(friend)
-  end
+#   def not_friends
+#     errors.add(:friend, 'is already added') if user.friends.include?(friend)
+#   end
 
-  def not_pending
-    errors.add(:friend, 'already requested friendship') if friend.pending_friends.include?(user)
-  end
+#   def not_pending
+#     errors.add(:friend, 'already requested friendship') if friend.pending_friends.include?(user)
+#   end
 
-  def accept
-    user.friends << friend
-    destroy
-  end
+#   def accept
+#     user.friends << friend
+#     destroy
+#   end
 
-end
+# end
